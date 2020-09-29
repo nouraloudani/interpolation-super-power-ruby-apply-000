8fc8914c04e1f4cea90e318af647076f6a18e33f
Noura Loudani <contact.nouraloudani@gmail.com>
2020-09-29 14:32:05 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,13 @@
 
 
-def display_rainbow(colors)
-  puts "#{display_rainbow[0]}, #{display_rainbow[1]}, #{display_rainbow[2]}, #{display_rainbow[3]}, #{display_rainbow[4]}, #{display_rainbow[5]}, #{display_rainbow[6]}"
+def display_rainbow()
+  puts "#{display_rainbow[0]}"
+  puts "#{display_rainbow[1]}"
+  puts "#{display_rainbow[2]}"
+  puts "#{display_rainbow[3]}"
+  puts "#{display_rainbow[4]}"
+  puts "#{display_rainbow[5]}"
+  puts "#{display_rainbow[6]}"
 end
 colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 display_rainbow(colors)
